e54e7ef9dc395add7b383f15670b3395729e7e24
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-05-25 10:40:31 -0500

    listile en vez del row

--- lib/screens/lago_screen.dart
+++ lib/screens/lago_screen.dart
@@ -7,7 +7,11 @@ class LagoScreen extends StatelessWidget {
     return Column(
       children: [
         Icon(icono, color: Colors.blue),
-        Text(texto, style: TextStyle(color: Colors.blue)),
+        SizedBox(height: 8),
+        Text(
+          texto,
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ],
     );
   }
@@ -24,21 +28,44 @@ class LagoScreen extends StatelessWidget {
             padding: const EdgeInsets.all(32.0),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [Text("Lago TITICACA"), Text("Peru - Puno")],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.star, color: Colors.orange),
-                        Text("41"),
-                      ],
-                    ),
-                  ],
+                ListTile(
+                  title: Text("Lago Titicaca"),
+                  subtitle: Text("Peru - Puno"),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.star, color: Colors.orange),
+                      Text("41"),
+                    ],
+                  ),
                 ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //   children: [
+                //     // Column(
+                //     //   crossAxisAlignment: CrossAxisAlignment.start,
+                //     //   children: [
+                //     //     Text(
+                //     //       "Lago TITICACA",
+                //     //       style: TextStyle(
+                //     //         fontWeight: FontWeight.bold,
+                //     //         fontSize: 16,
+                //     //       ),
+                //     //     ),
+                //     //     Text(
+                //     //       "Peru - Puno",
+                //     //       style: TextStyle(color: Colors.grey),
+                //     //     ),
+                //     //   ],
+                //     // ),
+                //     Row(
+                //       children: [
+                //         Icon(Icons.star, color: Colors.orange),
+                //         Text("41"),
+                //       ],
+                //     ),
+                //   ],
+                // ),
                 SizedBox(height: 32),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -47,6 +74,10 @@ class LagoScreen extends StatelessWidget {
                     botonIcono("ROUTE", Icons.route),
                     botonIcono("SHARE", Icons.share),
                   ],
+                ),
+                SizedBox(height: 32),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 ),
               ],
             ),
