9b525ea316320fe4278662ef3a919ad77d2213c3
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-05-25 11:25:48 -0500

    statefullwidget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int numero = 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Título del app")),
-      body: Column(
-        children: [
-          Text("El cuerpo del app"),
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(
-              "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(numero.toString(), style: TextStyle(fontSize: 50)),
+
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                numero += 1;
+                print("El nuevo valor de numero es: $numero");
+                setState(() {});
+              },
+              child: Text("Agregar 1 a numero"),
             ),
-            // child: Text("data"),
-          ),
-        ],
+            // CircleAvatar(
+            //   radius: 50,
+            //   backgroundImage: NetworkImage(
+            //     "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
