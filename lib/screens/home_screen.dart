a90fc310de2814438c7c68b536447e7f4025557b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-05-25 09:51:06 -0500

    circle avatar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,18 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Título del app")),
-      body: Column(children: [Text("El cuerpo del app")]),
+      body: Column(
+        children: [
+          Text("El cuerpo del app"),
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+              "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            ),
+            // child: Text("data"),
+          ),
+        ],
+      ),
     );
   }
 }
